5fbf8be3fd94e967c1802a4eb770bff0845d7b83
paigauresh <paigauresh@gmail.com>
2024-06-25 21:14:09 +0530

    navbar done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import '@/public/styles/Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-logo">
+          <Link href="/" className="navbar-logo-link">
+            Hearease
+          </Link>
+        </div>
+        <div className="navbar-links">
+          <a href="/download" className="btn-download">Download App</a>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
