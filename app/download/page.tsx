8c19d6d85eeba4ea654ee4e630b5be9c7321087b
paigauresh <paigauresh@gmail.com>
2024-07-03 00:57:51 +0530

    tailwind integration

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Sidebar from '@/components/Sidebar';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import styles from '@/public/styles/DownloadPage.module.css'; // Importing CSS Module
+import styles from '@/public/styles/DownloadPage.module.css';
 
 const DownloadPage = () => {
     return (
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Sidebar from '@/components/Sidebar';
 import Header from '@/components/Header';
 import MainContent from '@/components/Maincontent';
 import Footer from '@/components/Footer';
-import styles from '@/public/styles/Home.module.css'; // Importing CSS Module
+import styles from '@/public/styles/Home.module.css';
 
 const Home = () => {
   return (
