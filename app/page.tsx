aaff5466f2bfcf06863bc1797f7b628c5b75b410
paigauresh <paigauresh@gmail.com>
2024-07-08 00:08:21 +0530

    complete reset

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import Sidebar from '@/components/Sidebar';
-import Header from '@/components/Header';
-import MainContent from '@/components/Maincontent';
-import Footer from '@/components/Footer';
 
 const Home = () => {
   return (
-    <div className="flex h-screen font-sans">
-      <Sidebar />
-      <div className="flex flex-1 flex-col bg-gray-900 text-white">
-        <Header />
-        <MainContent />
-        <Footer />
-      </div>
+    <div>
+        
     </div>
   )
 }
