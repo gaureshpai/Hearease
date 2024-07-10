918b31548d1af5bb80ac3f2c3e46dc3670a00f77
paigauresh <paigauresh@gmail.com>
2024-07-10 16:40:37 +0530

    error resolve try

--- app/Library/page.tsx
+++ app/Library/page.tsx
@@ -29,9 +29,9 @@ const Playlist = () => {
   };
 
   return (
-    <div className="playlist-container bg-gray-100 p-4 rounded-md shadow-md">
-      <h2 className="text-2xl mb-4 text-gray-800">Playlist</h2>
-      <ul className="divide-y divide-gray-300">
+    <div>
+      <h2>Playlist</h2>
+      <ul>
         {tracks.map((track, index) => (
           <li key={index}>
             <Track track={track} onClick={() => handlePlayPause(track)} />
