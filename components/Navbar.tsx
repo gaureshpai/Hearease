170872d9568b3d5383c1a078b89d201bd73b0c02
paigauresh <paigauresh@gmail.com>
2024-07-28 03:08:44 +0530

    Navbar -> Download button in the media display turned none and multiple changes in styling Sidebar -> Icons added and also sidebar made as to be the row below the navbar in media view keepng the desktop view the same

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,14 +4,23 @@ import '@/public/styles/Navbar.css'
 
 const Navbar = () => {
   return (
-    <div className="Navbar bg-black text-white flex justify-between items-center p-2 w-full h-20">
-      <div className="heading text-4xl ml-8">
-        <Link href="/">HearEase</Link>
+    <nav className="bg-black text-white w-full h-[10vh] py-4 px-6 md:px-8 lg:px-12">
+      <div className="container mx-auto flex flex-col sm:flex-row md:justify-between items-center justify-center">
+        <div className="heading text-4xl font-bold mb-4 sm:mb-0">
+          <Link href="/" className="hover:text-gray-300 transition duration-300">
+            HearEase
+          </Link>
+        </div>
+        <div className="download text-base">
+          <Link
+            href="/download"
+            className="bg-white text-black px-4 py-2 rounded-full hover:bg-gray-200 transition duration-300"
+          >
+            Download App
+          </Link>
+        </div>
       </div>
-      <div className="download text-base mt-1 mr-8 p-2 ">
-        <Link href="/download">Download App</Link>
-      </div>   
-    </div>
+    </nav>
   )
 }
 
