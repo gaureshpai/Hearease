4f931dcb61f6abd29bc1bf0c75f496f63b3b9a53
shirha20 <harshiipoojary@gmail.com>
2024-11-15 20:51:26 +0530

    minor changes

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -6,16 +6,9 @@ import '@/public/styles/sidebar.css'
 
 const Sidebar = () => {
     return (
-        <aside className="bg-green-800 text-white lg:w-64 lg:h-[90vh] lg:overflow-y-auto lg:flex-shrink-0 transition-all duration-300 ease-in-out">
-            <nav className="flex lg:flex-col p-4  justify-center space-x-3 lg:space-x-0 lg:space-y-2 overflow-x-auto lg:overflow-x-visible">
-                <SidebarLink href="/" text="Home" icon={<FaHome />} />
-                <SidebarLink href="/About" text="About Tinnitus" icon={<FaInfoCircle />} />
-                <SidebarLink href="/Hearplay" text="Hearplay" icon={<FaHeadphones />} />
-                <SidebarLink href="/Hearelax" text="Hearelax" icon={<AiOutlineHeart />} />
-                <SidebarLink href="/Contributors" text="Contributors" icon={<FaUsers />} />
-                <div className="lg:hidden"><SidebarLink href="/download" text="Download app" icon={<FaDownload />} /></div>
-            </nav>
-        </aside>
+            <aside className="bg-green-800 text-white lg:w-35 lg:h-[90vh] lg:overflow-y-auto lg:flex-shrink-0 transition-all duration-300 ease-in-out">
+        
+            </aside>
     );
 };
 
@@ -25,7 +18,7 @@ interface SidebarLinkProps {
     icon: React.ReactNode;
 }
 
-const SidebarLink: React.FC<SidebarLinkProps> = ({ href, text, icon }) => (
+/*const SidebarLink: React.FC<SidebarLinkProps> = ({ href, text, icon }) => (
     <Link
         href={href}
         className="flex items-center justify-center lg:justify-start py-2 px-4 rounded-lg hover:bg-green-700 transition duration-200 ease-in-out whitespace-nowrap"
@@ -34,5 +27,5 @@ const SidebarLink: React.FC<SidebarLinkProps> = ({ href, text, icon }) => (
         <span className="hidden lg:inline">{text}</span>
     </Link>
 );
-
+*/
 export default Sidebar;
